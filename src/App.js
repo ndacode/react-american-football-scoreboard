45f2cea1d33b57fbbdff210abf0fe61d47c03177
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-07 22:45:01 -0400

    Added styling, player images, game and set scores auto adjust

--- src/App.js
+++ src/App.js
@@ -14,13 +14,39 @@ function App() {
   const [awayScore, setAwayScore] = useState(0);
   const [awayGameScore, setAwayGameScore] = useState(0);
   const [awaySetScore, setAwaySetScore] = useState(0);
+  
+
+  //  const handlePlayer = player => {setPlayer(player)};
+
+  const players = [
+    {FirstName: "Roger", LastName: "Federer", picture: {fed}},
+    {FirstName: "Serena", LastName: "Williams", picture: {fed}},
+    {FirstName: "Rafael", LastName: "Nadal", picture: {rafa3}},
+    {FirstName: "Novak", LastName: "Djokovic", picture: {fed}},
+    {FirstName: "Venus", LastName: "Williams", picture: {fed}},
+    {FirstName: "Nick", LastName: "Kyrgios", picture: {rafa3}},
+    {FirstName: "Naomi", LastName: "Osaka", picture: {fed}},
+    {FirstName: "Victoria", LastName: "Azarenka", picture: {rafa3}},
+    {FirstName: "Simona", LastName: "Halep", picture: {fed}}
+  ]
+   const [player, setPlayer] = useState(players[0]);
 
   return (
     <div className="container">
+      <div className="game">
+      <section className="buttons">
+      <div className="homeButtons">
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button className="homeButtons__touchdown" onClick = {() => (homeScore < 45 ? setHomeScore(homeScore + 15) : (setHomeScore(0), setHomeGameScore( homeGameScore + 1)))}> Point</button>
+
+          <button className="homeButtons__fieldGoal" onClick = {() => homeGameScore < 6 ? setHomeGameScore(homeGameScore + 1) : (setHomeGameScore(0), setHomeSetScore( homeSetScore + 1))}> Game</button>
+          <button className="homeButtons__fieldGoal" onClick = {() => homeSetScore < 6 ? setHomeSetScore(homeSetScore + 1) : setHomeSetScore(0)}> Set</button>
+        </div>
+      </section>
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <img src = {fed} alt ="Roger Federer"></img>
+            <img src = {fed} alt = "Roger Federer"></img>
             <h2 className="home__name">Roger Federer</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
@@ -30,8 +56,11 @@ function App() {
             <div className="home__score">{homeSetScore}</div>
           </div>
 
+          <div className="new-game">
+          <div className="timer">00:00</div>
+          <button className="new-game" onClick = {() => setPlayer(Math.floor((Math.random() * players.length)) + 1)}>New Players</button>
+          </div>
 
-          <div className="timer">00:03</div>
           <div className="away">
             <img src = {rafa3} alt ="Rafael Nadal"></img>
             <h2 className="away__name">Rafael Nadal</h2>
@@ -44,18 +73,13 @@ function App() {
       </section>
       
       <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick = {()=> setHomeScore(homeScore + 15)}>Federer Point</button>
-          <button className="homeButtons__fieldGoal" onClick = {() => setHomeGameScore(homeGameScore + 1)}>Federer Game</button>
-          <button className="homeButtons__fieldGoal" onClick = {() => setHomeSetScore(homeSetScore + 1)}>Federer Set</button>
-        </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick = {()=> setAwayScore(awayScore + 15)}>Rafa Point</button>
-          <button className="awayButtons__fieldGoal" onClick = {()=> setAwayGameScore(awayGameScore + 1)}>Rafa Game</button>
-          <button className="awayButtons__fieldGoal" onClick = {() => setAwaySetScore(awaySetScore + 1)}>Rafa Set</button>
+          <button className="awayButtons__touchdown" onClick = {() => awayScore < 45 ? setAwayScore(awayScore + 15) : (setAwayScore(0), setAwayGameScore( awayGameScore + 1))}> Point</button>
+          <button className="awayButtons__fieldGoal" onClick = {()=> awayGameScore < 6 ? setAwayGameScore(awayGameScore + 1): (setAwayGameScore(0), setAwaySetScore(awaySetScore + 1))}> Game</button>
+          <button className="awayButtons__fieldGoal" onClick = {() => awaySetScore < 6 ? setAwaySetScore(awaySetScore + 1) : setAwaySetScore(0)}> Set</button>
         </div>
       </section>
+      </div>
     </div>
   );
 }
